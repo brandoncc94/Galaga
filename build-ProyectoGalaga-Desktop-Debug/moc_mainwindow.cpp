313fd91241cbb9278dbfeaf14953d23508619988
brandoncc94 <brandoncc94@gmail.com>
2014-09-11 18:06:04 -0600

    File added

--- build-ProyectoGalaga-Desktop-Debug/moc_mainwindow.cpp
+++ build-ProyectoGalaga-Desktop-Debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[8];
-    char stringdata[90];
+    QByteArrayData data[10];
+    char stringdata[125];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -36,10 +36,13 @@ QT_MOC_LITERAL(3, 25, 16),
 QT_MOC_LITERAL(4, 42, 13),
 QT_MOC_LITERAL(5, 56, 7),
 QT_MOC_LITERAL(6, 64, 11),
-QT_MOC_LITERAL(7, 76, 12)
+QT_MOC_LITERAL(7, 76, 12),
+QT_MOC_LITERAL(8, 89, 21),
+QT_MOC_LITERAL(9, 111, 12)
     },
     "MainWindow\0startThreads\0\0executeAnimation\0"
     "executeBullet\0QLabel*\0executeTime\0"
+    "executeTrick\0executeEnemiesManager\0"
     "checkCollide\0"
 };
 #undef QT_MOC_LITERAL
@@ -50,7 +53,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -58,16 +61,20 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   39,    2, 0x0a,
-       3,    1,   40,    2, 0x0a,
-       4,    2,   43,    2, 0x0a,
-       6,    1,   48,    2, 0x0a,
-       7,    1,   51,    2, 0x0a,
+       1,    0,   49,    2, 0x0a,
+       3,    1,   50,    2, 0x0a,
+       4,    2,   53,    2, 0x0a,
+       6,    1,   58,    2, 0x0a,
+       7,    2,   61,    2, 0x0a,
+       8,    1,   66,    2, 0x0a,
+       9,    1,   69,    2, 0x0a,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void, QMetaType::Int,    2,
     QMetaType::Void, 0x80000000 | 5, QMetaType::Int,    2,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    2,    2,
     QMetaType::Void, QMetaType::Int,    2,
     QMetaType::Void, 0x80000000 | 5,    2,
 
@@ -83,7 +90,9 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 1: _t->executeAnimation((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 2: _t->executeBullet((*reinterpret_cast< QLabel*(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
         case 3: _t->executeTime((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 4: _t->checkCollide((*reinterpret_cast< QLabel*(*)>(_a[1]))); break;
+        case 4: _t->executeTrick((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 5: _t->executeEnemiesManager((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->checkCollide((*reinterpret_cast< QLabel*(*)>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
@@ -96,7 +105,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
                 *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QLabel* >(); break;
             }
             break;
-        case 4:
+        case 6:
             switch (*reinterpret_cast<int*>(_a[1])) {
             default: *reinterpret_cast<int*>(_a[0]) = -1; break;
             case 0:
@@ -132,13 +141,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 7;
     }
     return _id;
 }
--- build-ProyectoGalaga-Desktop-Debug/moc_threads.cpp
+++ build-ProyectoGalaga-Desktop-Debug/moc_threads.cpp
@@ -455,4 +455,211 @@ void TimeThread::timeRequest(int _t1)
     void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
     QMetaObject::activate(this, &staticMetaObject, 0, _a);
 }
+struct qt_meta_stringdata_TrickThread_t {
+    QByteArrayData data[3];
+    char stringdata[27];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_TrickThread_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_TrickThread_t qt_meta_stringdata_TrickThread = {
+    {
+QT_MOC_LITERAL(0, 0, 11),
+QT_MOC_LITERAL(1, 12, 12),
+QT_MOC_LITERAL(2, 25, 0)
+    },
+    "TrickThread\0trickRequest\0\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_TrickThread[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    2,   19,    2, 0x06,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    2,    2,
+
+       0        // eod
+};
+
+void TrickThread::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        TrickThread *_t = static_cast<TrickThread *>(_o);
+        switch (_id) {
+        case 0: _t->trickRequest((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (TrickThread::*_t)(int , int );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&TrickThread::trickRequest)) {
+                *result = 0;
+            }
+        }
+    }
+}
+
+const QMetaObject TrickThread::staticMetaObject = {
+    { &QThread::staticMetaObject, qt_meta_stringdata_TrickThread.data,
+      qt_meta_data_TrickThread,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *TrickThread::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *TrickThread::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_TrickThread.stringdata))
+        return static_cast<void*>(const_cast< TrickThread*>(this));
+    return QThread::qt_metacast(_clname);
+}
+
+int TrickThread::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QThread::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void TrickThread::trickRequest(int _t1, int _t2)
+{
+    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+struct qt_meta_stringdata_EnemiesManager_t {
+    QByteArrayData data[3];
+    char stringdata[39];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_EnemiesManager_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_EnemiesManager_t qt_meta_stringdata_EnemiesManager = {
+    {
+QT_MOC_LITERAL(0, 0, 14),
+QT_MOC_LITERAL(1, 15, 21),
+QT_MOC_LITERAL(2, 37, 0)
+    },
+    "EnemiesManager\0enemiesManagerRequest\0"
+    "\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_EnemiesManager[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   19,    2, 0x06,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::Int,    2,
+
+       0        // eod
+};
+
+void EnemiesManager::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        EnemiesManager *_t = static_cast<EnemiesManager *>(_o);
+        switch (_id) {
+        case 0: _t->enemiesManagerRequest((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (EnemiesManager::*_t)(int );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&EnemiesManager::enemiesManagerRequest)) {
+                *result = 0;
+            }
+        }
+    }
+}
+
+const QMetaObject EnemiesManager::staticMetaObject = {
+    { &QThread::staticMetaObject, qt_meta_stringdata_EnemiesManager.data,
+      qt_meta_data_EnemiesManager,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *EnemiesManager::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *EnemiesManager::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_EnemiesManager.stringdata))
+        return static_cast<void*>(const_cast< EnemiesManager*>(this));
+    return QThread::qt_metacast(_clname);
+}
+
+int EnemiesManager::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QThread::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void EnemiesManager::enemiesManagerRequest(int _t1)
+{
+    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
 QT_END_MOC_NAMESPACE
